420610517baa08f6570a041626e98d5c126ac0cc
LisandroGarcia <legarcia@obralapiedad.com.ar>
2026-09-17 09:26:49 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('mouseenter', () => {
-    card.classList.add('is-active');
-  });
-
-  card.addEventListener('mouseleave', () => {
-    card.classList.remove('is-active');
-  });
-});
